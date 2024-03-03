a3e2a5765a9374ceb6aa068bcad32241257ad6bd
Chloeliu16 <158484224+Chloeliu16@users.noreply.github.com>
2024-03-03 13:23:26 -0500

    add CrossOrigin to allow frontend access.

--- src/main/java/com/example/arst5backend/controller/AirportController.java
+++ src/main/java/com/example/arst5backend/controller/AirportController.java
@@ -3,6 +3,7 @@ package com.example.arst5backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.example.arst5backend.model.Airport;
 import com.example.arst5backend.repository.AirportRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AirportController {
 
     private AirportRepository airportRepository;
