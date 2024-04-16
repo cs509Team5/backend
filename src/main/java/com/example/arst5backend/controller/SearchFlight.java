9215774981c322de1e60d501610d13db61ead264
Mario Zyla <zylamario@icloud.com>
2024-04-15 22:29:59 -0400

    making search a post request

--- src/main/java/com/example/arst5backend/controller/SearchFlight.java
+++ src/main/java/com/example/arst5backend/controller/SearchFlight.java
@@ -7,7 +7,9 @@ import dto.FlightInfo;
 import dto.SearchRequest;
 import dto.SearchResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,40 +21,40 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/search")
+@CrossOrigin(origins = "*")
 public class SearchFlight {
     private final SearchStrategy searchStrategy;
 
     @Autowired
-    public SearchFlight(SearchStrategy searchStrategy){
+    public SearchFlight(SearchStrategy searchStrategy) {
         this.searchStrategy = searchStrategy;
     }
 
-    @GetMapping
+    @PostMapping
     public SearchResponse search(@RequestBody SearchRequest searchRequest) {
         System.out.println("Search request: " + searchRequest);
 
-            List<FlightInfo> allDepartureFlights = searchStrategy.evaluate(
-                    searchRequest.getDepartureAirport(),
+        List<FlightInfo> allDepartureFlights = searchStrategy.evaluate(
+                searchRequest.getDepartureAirport(),
+                searchRequest.getArrivalAirport(),
+                searchRequest.getNumberOfStopover(),
+                searchRequest.isAcceptEconomy(),
+                searchRequest.isAcceptFirstClass(),
+                searchRequest.getDepartureDate());
+        SearchResponse searchResponse = new SearchResponse();
+        searchResponse.setDepartureFlights(allDepartureFlights);
+        if (searchRequest.getReturnDate() != null) {
+            // System.out.println("number of stopover: " +
+            // searchRequest.getNumberOfStopover());
+            List<FlightInfo> allReturnFlights = searchStrategy.evaluate(
                     searchRequest.getArrivalAirport(),
+                    searchRequest.getDepartureAirport(),
                     searchRequest.getNumberOfStopover(),
                     searchRequest.isAcceptEconomy(),
                     searchRequest.isAcceptFirstClass(),
-                    searchRequest.getDepartureDate()
-            );
-            SearchResponse searchResponse = new SearchResponse();
-            searchResponse.setDepartureFlights(allDepartureFlights);
-            if (searchRequest.getReturnDate() != null) {
-    //            System.out.println("number of stopover: " + searchRequest.getNumberOfStopover());
-                List<FlightInfo> allReturnFlights = searchStrategy.evaluate(
-                        searchRequest.getArrivalAirport(),
-                        searchRequest.getDepartureAirport(),
-                        searchRequest.getNumberOfStopover(),
-                        searchRequest.isAcceptEconomy(),
-                        searchRequest.isAcceptFirstClass(),
-                        searchRequest.getReturnDate()
-                );
-                searchResponse.setReturnFlights(allReturnFlights);
-            }
+                    searchRequest.getReturnDate());
+            searchResponse.setReturnFlights(allReturnFlights);
+        }
         return searchResponse;
     }
 }
