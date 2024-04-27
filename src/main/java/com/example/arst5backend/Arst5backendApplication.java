901aa5749ed2750938d35648cae0ab2368632413
Mario Zyla <zylamario@icloud.com>
2024-04-27 19:22:32 -0400

    manual configs

--- src/main/java/com/example/arst5backend/Arst5backendApplication.java
+++ src/main/java/com/example/arst5backend/Arst5backendApplication.java
@@ -2,9 +2,8 @@ package com.example.arst5backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class Arst5backendApplication {
 
 	public static void main(String[] args) {
